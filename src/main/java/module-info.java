b300f64a5a8271c94068dd8c49b25c3acc931d87
yildiz-online <vandenborre.gregory@hotmail.fr>
2019-08-22 21:27:00 +0300

    [WIP] Support module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module be.yildizgames.module.window.javafx {
+
+    requires be.yildizgames.module.window;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
+    requires com.sun.jna.platform;
+    requires com.sun.jna;
+
+
+}
