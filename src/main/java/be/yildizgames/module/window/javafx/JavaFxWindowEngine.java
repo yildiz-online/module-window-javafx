611c79616586822bbab4a96a1f5d4e3ac77568a6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2023-04-20 10:40:14 +0200

    [WIP] Move elements related to screen to dedicated package.

--- src/main/java/be/yildizgames/module/window/javafx/JavaFxWindowEngine.java
+++ src/main/java/be/yildizgames/module/window/javafx/JavaFxWindowEngine.java
@@ -1,33 +1,24 @@
 /*
- * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
- *
- *  Copyright (c) 2019 Grégory Van den Borre
- *
- *  More infos available: https://engine.yildiz-games.be
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- *  documentation files (the "Software"), to deal in the Software without restriction, including without
- *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- *  of the Software, and to permit persons to whom the Software is furnished to do so,
- *  subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all copies or substantial
- *  portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
- *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
- *
+ This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ Copyright (c) 2019-2023 Grégory Van den Borre
+ More infos available: https://engine.yildiz-games.be
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright
+ notice and this permission notice shall be included in all copies or substantial portions of the  Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package be.yildizgames.module.window.javafx;
 
 import be.yildizgames.module.window.BaseWindowEngine;
-import be.yildizgames.module.window.Cursor;
+import be.yildizgames.module.window.screen.Cursor;
 import be.yildizgames.module.window.RegisteredView;
-import be.yildizgames.module.window.ScreenSize;
+import be.yildizgames.module.window.screen.ScreenSize;
 import be.yildizgames.module.window.WindowThreadManager;
 import be.yildizgames.module.window.input.WindowInputListener;
 import be.yildizgames.module.window.javafx.widget.JavaFxWindowShell;
