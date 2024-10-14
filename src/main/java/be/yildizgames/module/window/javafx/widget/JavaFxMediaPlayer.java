c5839a26062bf1347f3da437133798630e021519
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-10-14 07:20:01 +0200

    [WIP] Fix class scope.

--- src/main/java/be/yildizgames/module/window/javafx/widget/JavaFxMediaPlayer.java
+++ src/main/java/be/yildizgames/module/window/javafx/widget/JavaFxMediaPlayer.java
@@ -39,7 +39,7 @@ import java.nio.file.Path;
 /**
  * @author Grégory Van den Borre
  */
-public class JavaFxMediaPlayer implements WindowMediaPlayer {
+class JavaFxMediaPlayer implements WindowMediaPlayer {
 
     private MediaPlayerStrategy current;
 
