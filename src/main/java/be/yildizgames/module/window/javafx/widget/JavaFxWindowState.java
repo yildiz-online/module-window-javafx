881300f2c2311dce445a66c636ca8da7e6c218f3
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-10-24 17:12:59 +0200

    [WIP] Find from id in template files.

--- src/main/java/be/yildizgames/module/window/javafx/widget/JavaFxWindowState.java
+++ src/main/java/be/yildizgames/module/window/javafx/widget/JavaFxWindowState.java
@@ -4,11 +4,37 @@ import be.yildizgames.module.color.Color;
 import be.yildizgames.module.coordinates.Coordinates;
 import be.yildizgames.module.window.input.KeyboardListener;
 import be.yildizgames.module.window.javafx.widget.experimental.JavaFxVirtualKeyboard;
-import be.yildizgames.module.window.widget.*;
+import be.yildizgames.module.window.widget.WindowButton;
+import be.yildizgames.module.window.widget.WindowButtonText;
+import be.yildizgames.module.window.widget.WindowCanvas;
+import be.yildizgames.module.window.widget.WindowCheckBox;
+import be.yildizgames.module.window.widget.WindowDropdown;
+import be.yildizgames.module.window.widget.WindowImage;
+import be.yildizgames.module.window.widget.WindowImageProvider;
+import be.yildizgames.module.window.widget.WindowInputBox;
+import be.yildizgames.module.window.widget.WindowMediaPlayer;
+import be.yildizgames.module.window.widget.WindowMenuBar;
+import be.yildizgames.module.window.widget.WindowMenuBarElementDefinition;
+import be.yildizgames.module.window.widget.WindowModal;
+import be.yildizgames.module.window.widget.WindowModalFile;
+import be.yildizgames.module.window.widget.WindowNotificationPane;
+import be.yildizgames.module.window.widget.WindowProgressBar;
+import be.yildizgames.module.window.widget.WindowShape;
+import be.yildizgames.module.window.widget.WindowShell;
+import be.yildizgames.module.window.widget.WindowState;
+import be.yildizgames.module.window.widget.WindowTextArea;
+import be.yildizgames.module.window.widget.WindowTextLine;
+import be.yildizgames.module.window.widget.WindowToggle;
+import be.yildizgames.module.window.widget.WindowTreeElement;
+import be.yildizgames.module.window.widget.WindowTreeRoot;
+import be.yildizgames.module.window.widget.WindowWidgetCreator;
 import be.yildizgames.module.window.widget.experimental.VirtualKeyboard;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
@@ -17,6 +43,11 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 class JavaFxWindowState implements WindowState {
 
@@ -29,6 +60,12 @@ class JavaFxWindowState implements WindowState {
     private final WindowImageProvider imageProvider;
 
     private final JavaFxWindowShell shell;
+
+    private final Map<String, WindowTextLine> labels = new HashMap<>();
+    private final Map<String, WindowImage> images = new HashMap<>();
+    private final Map<String, WindowShape> shapes = new HashMap<>();
+    private final Map<String, WindowButton> buttons = new HashMap<>();
+
 
     JavaFxWindowState(Pane pane, WindowImageProvider imageProvider, JavaFxWindowShell shell, Scene scene) {
         this.pane = pane;
@@ -74,6 +111,58 @@ class JavaFxWindowState implements WindowState {
         this.pane.setBackground(new Background(myBI));
 
         return this;
+    }
+
+    @Override
+    public final Optional<WindowTextLine> findTextLine(String id) {
+        var result = this.labels.get(id);
+        if(result == null) {
+            var item = this.pane.lookup(id);
+            if(item != null && item.getClass() == Label.class) {
+                 result = new JavaFxLabel((Label)item, this.shell);
+                 this.labels.put(id, result);
+            }
+        }
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public final Optional<WindowImage> findImage(String id) {
+        var result = this.images.get(id);
+        if(result == null) {
+            var item = this.pane.lookup(id);
+            if(item != null && item.getClass() == ImageView.class) {
+                result = new JavaFxImage((ImageView)item, this.imageProvider);
+                this.images.put(id, result);
+            }
+        }
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Optional<WindowShape> findShape(String id) {
+        var result = this.shapes.get(id);
+        if(result == null) {
+            var item = this.pane.lookup(id);
+            if(item != null && item.getClass() == Rectangle.class) {
+                result = new JavaFxShape((Rectangle)item);
+                this.shapes.put(id, result);
+            }
+        }
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Optional<WindowButton> findButton(String id) {
+        var result = this.buttons.get(id);
+        if(result == null) {
+            var item = this.pane.lookup(id);
+            if(item != null && item.getClass() == Button.class) {
+                result = new JavaFxButton((Button)item, this.imageProvider);
+                this.buttons.put(id, result);
+            }
+        }
+        return Optional.ofNullable(result);
     }
 
     @Override
